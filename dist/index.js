5733b91e540fbfba4008f2c5ecec8823ca6ef987
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-12-30 10:34:38 +0600

    Journey to TypeScript

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let sales = 50000;
+let course = 'TypeScript';
+let is_available = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+render('hello');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let sales = 50_000;
+let course = 'TypeScript';
+let is_available = true;
+
+let level;
+
+function render(document){
+    console.log(document);
+    
+}
+render('hello')
